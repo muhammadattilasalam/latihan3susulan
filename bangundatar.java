a9ca14de6675c244240425090ea8fd4952a8b1a3
muhammadattilasalam <xdoblehx@gmail.com>
2025-06-24 16:46:48 +0700

    membuat mutator

--- bangundatar.java
+++ bangundatar.java
@@ -11,5 +11,15 @@ public class BangunDatar {
     double alas;
     double tinggi;
     double jariJari;
-    double hasil;
+    double hasil;
+
+    // Method untuk menghitung luas segitiga
+    public void hitungLuasSegitiga() {
+        hasil = 0.5 * alas * tinggi;
+    }
+
+    // Method untuk menghitung luas lingkaran
+    public void hitungLuasLingkaran() {
+        hasil = 3.14 * jariJari * jariJari;
+    }
 }
