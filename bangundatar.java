e075b7b33ded4a52c38debc85ab900cac66b71ec
muhammadattilasalam <xdoblehx@gmail.com>
2025-06-24 16:38:50 +0700

    initial commit

---
+++ bangundatar.java
@@ -0,0 +1,11 @@
+package latihan3susulan;
+
+/*
+ * Nama : muhammad attila salam
+ * NPM  : 2310010635
+ * Kelas: 4a
+ */
+
+public class BangunDatar {
+    // Class kosong sesuai instruksi (hanya komentar biodata)
+}
